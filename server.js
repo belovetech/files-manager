1ea00f83e469bfb6ccf598cbebf7fb51dcb0c115
belovetech <belovetech@gmail.com>
2023-03-06 14:07:25 +0100

    [Feat]: configure server

---
+++ server.js
@@ -0,0 +1,15 @@
+import router from './routes';
+
+const express = require('express');
+
+const PORT = process.env.PORT || '5000';
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`);
+});
+
+export default app;
