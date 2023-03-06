a634e11db837fb628ad523af98b789a819ada503
belovetech <belovetech@gmail.com>
2023-03-06 07:49:13 +0100

    [Update]: connect mongodb database

--- utils/db.js
+++ utils/db.js
@@ -7,29 +7,29 @@ const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
-    constructor() {
-        MongoClient.connect(url, (err, client) => {
-            if (!err) {
-                this.db = client.db(DATABASE);
-            }
-            this.db = false;
-        });
-    }
+  constructor() {
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+        this.db = client.db(DATABASE);
+      }
+      this.db = false;
+    });
+  }
 
-    isAlive() {
-        if (this.db) {
-            return true;
-        }
-        return false;
+  isAlive() {
+    if (this.db) {
+      return true;
     }
+    return false;
+  }
 
-    async nbUsers() {
-        return this.db.collection('users').countDocuments();
-    }
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-        return this.db.collection('files').countDocuments();
-    }
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
