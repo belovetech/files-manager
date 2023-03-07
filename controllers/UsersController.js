64db08aa606fbc9e466c4c3cc2e0c790c7fa4350
belovetech <belovetech@gmail.com>
2023-03-07 14:06:55 +0100

    [Update]: hash password used in user controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,5 @@
 import dbClient from '../utils/db';
-
-const sha1 = require('sha1');
+import hashPassword from '../utils/hashPassword';
 
 class UsersController {
   static async postNew(req, res) {
@@ -19,11 +18,9 @@ class UsersController {
       return res.status(400).send({ error: 'Already exist' });
     }
 
-    const hashPassword = sha1(password);
-
     const newUser = {
       email,
-      password: hashPassword,
+      password: hashPassword(password),
     };
 
     await dbClient.db.collection('users').insertOne(newUser);
