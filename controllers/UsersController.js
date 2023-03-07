a1ea72ba1be0197db7211bace93122905daac315
belovetech <belovetech@gmail.com>
2023-03-07 14:24:53 +0100

    [Fix]: checker error

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,16 +6,17 @@ class UsersController {
     const { email, password } = req.body;
 
     if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
 
     if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
+
     const user = await dbClient.db.collection('users').findOne({ email });
 
     if (user) {
-      return res.status(400).send({ error: 'Already exist' });
+      return res.status(400).json({ error: 'Already exist' });
     }
 
     const newUser = {
@@ -23,12 +24,10 @@ class UsersController {
       password: hashPassword(password),
     };
 
-    await dbClient.db.collection('users').insertOne(newUser);
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+    const id = result.insertedId.toString();
 
-    return res.status(200).send({
-      id: newUser._id,
-      email,
-    });
+    return res.status(200).json({ id, email });
   }
 }
 
