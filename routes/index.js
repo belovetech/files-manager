054033975d9809a9bee74c9da1b7d8ae25a230b4
belovetech <belovetech@gmail.com>
2023-03-07 14:00:22 +0100

    [Update]: user route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const express = require('express');
 
@@ -7,5 +8,8 @@ const router = express.Router();
 
 router.route('/status').get(AppController.getStatus);
 router.route('/stats').get(AppController.getStats);
+
+// users routes
+router.route('/users').post(UsersController.postNew);
 
 export default router;
