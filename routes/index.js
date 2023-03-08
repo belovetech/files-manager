06eab1fa904c7d74fe9e6fb4247b3825c1b78a90
belovetech <belovetech@gmail.com>
2023-03-08 20:43:52 +0100

    [Update]: resources endpoints

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,6 @@ router.get('/files', FilesController.getIndex);
 router.get('/files/:id', FilesController.getShow);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getData);
+// router.get('/files/:id/data', FilesController.getData);
 
 export default router;
